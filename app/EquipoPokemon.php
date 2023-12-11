583b13d120e34ef896e03804b8d2da5f251da104
desarrollo telepagos <109679456+desarrollo-telepagos@users.noreply.github.com>
2023-12-11 19:06:14 -0300

    Initial Commit

---
+++ app/EquipoPokemon.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class EquipoPokemon extends Model
+{
+    public $timestamps = false;
+
+    public $table = "equipos_pokemones";
+
+    protected $fillable = ['id_equipos', 'id_pokemones', 'orden'];
+}
